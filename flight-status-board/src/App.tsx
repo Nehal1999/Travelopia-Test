675a2cca9f2dc1f2bef44c2357aec8952cf0fb82
Nehal1999 <banasthalinehal1999@gmail.com>
2024-03-22 19:17:28 +0530

    app root component with routing

--- flight-status-board/src/App.tsx
+++ flight-status-board/src/App.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import FlightsDetails from './pages/flight-details/flightsDetails';
+import NavbarComponent from './components/navbar/navbar';
+import FlightInfo from './pages/flight-info/flightInfo';
+import TravelopiaHomePage from './pages/travelopia-homepage/travelopiaHomePage';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path : "/",
+      element : <TravelopiaHomePage/>
+    },
+    {
+      path : "/flights",
+      element : <FlightsDetails/>
+    },
+    {
+      path : "/flights/:id",
+      element : <FlightInfo/>
+    }
+  ])
+
   return (
-    <></>
+    <> 
+    <>
+    <NavbarComponent/>
+    </>
+    <div className='container'>
+    <RouterProvider router={router}></RouterProvider>
+    </div>
+    </>
   );
 }
 
